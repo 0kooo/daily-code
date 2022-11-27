9aa115685fc807d6965e4a6a0797c9db74a4f4f5
360619623@qq.com <360619623@qq.com>
2022-11-27 09:18:32 +0800

    commit

---
+++ a202211/src/day52/Text2/Solution.java
@@ -0,0 +1,19 @@
+package day52.Text2;
+
+/*
+public class Solution extends GuessGame{
+    public int guessNumber(int n) {
+        int left = 1;
+        int right = n;
+        while(left < right){
+            int mid =left + (right - left) / 2;
+            if(guess(mid) <= 0){
+                right = mid;
+            }else{
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
+*/
