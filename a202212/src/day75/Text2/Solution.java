d95958eed307ddd219cd10249afa5a3474072926
360619623@qq.com <360619623@qq.com>
2022-12-20 20:23:51 +0800

    commit

---
+++ a202212/src/day75/Text2/Solution.java
@@ -0,0 +1,25 @@
+package day75.Text2;
+
+import java.util.Arrays;
+
+class Solution {
+    public int minimumSize(int[] nums, int maxOperations) {
+        int left = 1;
+        int right = Arrays.stream(nums).max().getAsInt();
+        int ans = 0;
+        while(left <= right){
+            int y = (left + right) / 2;
+            long ops = 0;
+            for (int x : nums) {
+                ops += (x - 1) / y;
+            }
+            if(ops <= maxOperations){
+                ans = y;
+                right = y - 1;
+            }else{
+                left = y + 1;
+            }
+        }
+        return ans;
+    }
+}
