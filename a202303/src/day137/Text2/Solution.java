73b9e9a7713dbe60be96fce4427a16727685d8fe
360619623@qq.com <360619623@qq.com>
2023-03-01 19:09:26 +0800

    commit

--- a202303/src/day137/Text2/Solution.java
+++ a202303/src/day137/Text2/Solution.java
@@ -1,2 +1,31 @@
-package day137.Text2;public class Solution {
+package day137.Text2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Integer> selfDividingNumbers(int left, int right) {
+        List<Integer> res = new ArrayList<>();
+        for (int i = left; i <= right; i++) {
+            if(getDividingNumber(i)){
+                res.add(i);
+            }
+        }
+        return res;
+    }
+
+    public boolean getDividingNumber(int x){
+        int temp = x;
+        while(temp != 0){
+            int ge = temp % 10;
+            temp /= 10;
+            if(ge == 0){
+                return false;
+            }
+            if(x % ge != 0){
+                return false;
+            }
+        }
+        return true;
+    }
 }
