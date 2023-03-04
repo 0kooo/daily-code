be6372305268562e4fd55aa1bc12c5be2e867a27
360619623@qq.com <360619623@qq.com>
2023-03-04 16:41:44 +0800

    commit

--- a202303/src/day140/Text1/Solution.java
+++ a202303/src/day140/Text1/Solution.java
@@ -1,2 +1,19 @@
-package day140.Text1;public class Solution {
+package day140.Text1;
+
+import java.util.*;
+
+class Solution {
+    public int uniqueMorseRepresentations(String[] words) {
+        HashSet<String> set = new HashSet<>();
+        String[] arr = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        for (String word : words) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < word.length(); i++) {
+                char c = word.charAt(i);
+                sb.append(arr[c - 97]);
+            }
+            set.add(sb.toString());
+        }
+        return set.size();
+    }
 }
