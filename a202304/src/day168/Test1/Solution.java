691d3b2ee70c41480bbdedc772304bbcc390d47a
360619623@qq.com <360619623@qq.com>
2023-04-01 09:29:40 +0800

    commit

--- a202304/src/day168/Test1/Solution.java
+++ a202304/src/day168/Test1/Solution.java
@@ -1,2 +1,48 @@
-package day168.Test1;public class Solution {
+package day168.Test1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//831. 隐藏个人信息
+class Solution {
+    public String maskPII(String s) {
+        StringBuilder res = new StringBuilder();
+        int len = s.length();
+        if(s.contains("@")){/*s.matches("[a-zA-Z]+@[a-zA-Z]+\\.[a-zA-Z]+")*/
+            s = s.toLowerCase();
+            String[] arr = s.split("@");
+            res.append(arr[0].charAt(0)).append("*****").append(arr[0].charAt(arr[0].length() - 1)).append("@");
+            res.append(arr[1].toLowerCase());
+        }else{
+            List<Character> list = new ArrayList<>();
+            for (int i = 0; i < len; i++) {
+                char c = s.charAt(i);
+                if(c >= '0' && c <= '9'){
+                    list.add(c);
+                }
+            }
+            switch (list.size()) {
+                case 10 -> {
+                    res.append("***-***-");
+                    break;
+                }
+                case 11 -> {
+                    res.append("+*-***-***-");
+                    break;
+                }
+                case 12 -> {
+                    res.append("+**-***-***-");
+                    break;
+                }
+                case 13 -> {
+                    res.append("+***-***-***-");
+                    break;
+                }
+            }
+            for (int i = list.size() - 4; i < list.size(); i++) {
+                res.append(list.get(i));
+            }
+        }
+        return res.toString();
+    }
 }
--- a202304/src/day168/Test3/Solution.java
+++ a202304/src/day168/Test3/Solution.java
@@ -1,2 +1,24 @@
-package day168.Test3;public class Solution {
+package day168.Test3;
+
+import java.util.HashSet;
+import java.util.Set;
+
+//929. 独特的电子邮件地址
+class Solution {
+    public int numUniqueEmails(String[] emails) {
+        Set<String> set = new HashSet<>();
+        for (String email : emails) {
+            String[] arr = email.split("@");
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < arr[0].length(); i++) {
+                char c = arr[0].charAt(i);
+                if(c == '+') break;
+                if(c >= 'a' && c <= 'z') sb.append(c);
+            }
+            sb.append("@").append(arr[1]);
+            String s = sb.toString();
+            set.add(s);
+        }
+        return set.size();
+    }
 }
